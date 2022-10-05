7945d1259eceb3a94759282251989cf906de6a09
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-10-04 21:05:12 -0500

    Cambios3

--- Aplicacion.js
+++ Aplicacion.js
@@ -2,3 +2,16 @@ function busqueda(){
     document.getElementById("menu1").style.display = "none";
     document.getElementById("menu2").style.display = "block";
 }
+
+
+let zonas = [
+	{
+        "nombre": "santa fe",
+        "desde": "calle 13 con 1",
+        "hasta": "calle 24 xon caracas",
+        "delitos-comunes":[
+            "hurto","asesinato"
+        ],
+        "horario":"22:00 - +05:00"
+	}
+];
