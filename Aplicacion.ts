5cd4c1d3cdc2b8e4d1f184a0b0a0dc0d27cf7ee1
MariVang <villamizarangaritamariana@artesyletras.edu.co>
2022-11-15 10:19:43 -0500

    Ts changes

---
+++ Aplicacion.ts
@@ -0,0 +1,14 @@
+export interface zonas{
+
+    localidad: string,
+    sector: string,
+    desde: string,
+    hasta: string,
+    delitosComunes:delitosComunes,
+    horario:string;
+    
+}
+
+export interface delitosComunes{
+
+}
